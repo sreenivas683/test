fd584b4044befc8be694017ba46ba226dd736ef9
sreenivas683@gmail.com <sreenivas683@gmail.com>
2023-04-07 06:33:57 +0000

    06-04-2023

--- models/DIM_CUSTOMER.sql
+++ models/DIM_CUSTOMER.sql
@@ -1,11 +1,6 @@
-with
-    customers as (
-        select id as customer_id, first_name, last_name from raw.jaffle_shop.customers
-    ),
-    orders as (
-        select id as order_id, user_id as customer_id, order_date, status
-        from raw.jaffle_shop.orders
-    ),
+{{ config(materialized="table") }}
+with customers as (select * from {{ ref("stg_customers") }}),
+    orders as (select * from {{ ref("stg_orders") }}),
     customer_orders as (
         select
             customer_id,
@@ -26,6 +21,4 @@ with
         from customers
         left join customer_orders using (customer_id)
     )
-
-select *
-from final
+select * from final
