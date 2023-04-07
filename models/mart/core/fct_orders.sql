2c9b84dba3e5a038a87a56b0ce99ae8ae3053b74
sreenivas683@gmail.com <sreenivas683@gmail.com>
2023-04-07 07:47:16 +0000

    07-04-2023

---
+++ models/mart/core/fct_orders.sql
@@ -0,0 +1,9 @@
+with orders as 
+(select * from {{ ref("stg_orders") }}),
+payments as 
+(select * from {{ ref("stg_payment") }}),
+    fct_orders as (
+        select order_id,payment_id,amount
+        from orders o
+        left join payments p on o.order_id = p.order_id)
+select * from fct_orders
---
+++ models/staging/stripe/stg_payment.sql
@@ -0,0 +1,6 @@
+with
+    payments as (
+        select id as payment_id, orderid as order_id, paymentmethod, status, amount, created
+        from raw.stripe.payment
+    )
+select * from payments
