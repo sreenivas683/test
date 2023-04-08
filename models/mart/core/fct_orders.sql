99ac26b26dbcb18ef65251da51987be034b7d98d
sreenivas683@gmail.com <sreenivas683@gmail.com>
2023-04-08 09:59:55 +0000

    fact

--- models/mart/core/fct_orders.sql
+++ models/mart/core/fct_orders.sql
@@ -3,7 +3,7 @@ with orders as
 payments as 
 (select * from {{ ref("stg_payment") }}),
     fct_orders as (
-        select order_id,payment_id,amount
+        select o.order_id,payment_id,amount
         from orders o
         left join payments p on o.order_id = p.order_id)
 select * from fct_orders
