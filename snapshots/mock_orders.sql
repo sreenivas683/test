2d4a2da77da02f1da0f14b3d64e806e6a221f2fa
sreenivas vajinepally <sreenivasvajinepally@gmail.com>
2023-05-05 07:53:51 +0000

    05052023

--- snapshots/mock_orders.sql
+++ snapshots/mock_orders.sql
@@ -4,7 +4,7 @@
 {{
     config(
         target_database='pc_dbt_db',
-        target_schema=new_schema,
+        target_schema='dbt_amorton',
         unique_key='order_id',
         strategy='timestamp',
         updated_at='updated_at',
